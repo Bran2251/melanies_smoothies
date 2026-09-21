0fc98d0abc57670e88f05d3fc3033192830f7d99
Bran2251 <brandon-smith251@hotmail.com>
2026-09-20 21:27:38 -0500

    Create requirements.py

---
+++ requirements.py
@@ -0,0 +1,36 @@
+# Import python packages
+import streamlit as st
+import os
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
+
+# Write directly to the app
+st.title(f":cup_with_straw: Pendin orders :cup_with_straw:")
+st.write(
+  "orders that need to be filled"
+)
+
+
+
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.orders") \
+    .filter(col("ORDER_FILLED") == 0) \
+    .collect()
+editable_df = st.data_editor(my_dataframe)
+
+
+
+if my_dataframe:
+    editable_df = st.data_editor(my_dataframe)
+    submitted= st.button('submit')
+    if submitted:
+        st.success("someone click the button", icon = '👍')
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+        og_dataset.merge(edited_dataset
+                         , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                         , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                        )
+else: st.success('There are no pendin orders')

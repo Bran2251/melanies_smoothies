dc0096588c56087e3fa4426ef9a1d22cb653b223
Bran2251 <brandon-smith251@hotmail.com>
2026-09-20 22:57:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,9 @@ if Ingredient_list:
 
 import requests
 
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
+url = "https://my.smoothiefroot.com/api/fruit/watermelon"
 
-st.text(smoothiefroot_response.text)
+response = requests.get(url)
+
+st.write("Status code:", response.status_code)
+st.write("Response:", response.text)
